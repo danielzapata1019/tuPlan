1f7e47b69586386b239f645b629d9d8d4dae5327
Daniel Zapata <danielzapata1019@gmail.com>
2018-05-23 21:37:28 -0500

    primer intento slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './home/slider/slider.component';
+import { RouteRoutingModule } from './route/route-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouteRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
